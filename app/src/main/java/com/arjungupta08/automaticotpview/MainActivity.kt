7b59a6d405dd1ade9aa93859eb2f146270c4ee2c
Arjun Gupta <arjungupta0817@gmail.com>
2024-04-16 22:24:31 +0530

    initial commit

---
+++ app/src/main/java/com/arjungupta08/automaticotpview/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.arjungupta08.automaticotpview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
